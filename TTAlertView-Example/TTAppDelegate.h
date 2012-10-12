93b35e105ea78c51d9f95f5ebd80cee346899478
Duncan Lewis <duncan@twotoasters.com>
2012-10-12 11:20:19 -0400

    Added readme, setup project

---
+++ TTAlertView-Example/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  TTAlertView
+//
+//  Created by Duncan Lewis on 10/12/12.
+//  Copyright (c) 2012 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TTAlertView-Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TTAlertView
+//
+//  Created by Duncan Lewis on 10/12/12.
+//  Copyright (c) 2012 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTAppDelegate class]));
+    }
+}
